c7d665f87864c028398a39de95d9468ed90a0640
justint <justint@2f1e4d80-d51e-0410-82d6-c02f1edca786>
2009-01-15 08:48:39 +0000

    Removed use of MAPI for emailing. Add test facility to service. Various UI changes and validations.
    git-svn-id: https://svn.korbitec.int/svn/master/Products/GhostWare/GhostService@35404 2f1e4d80-d51e-0410-82d6-c02f1edca786

---
+++ trunk/GhostService/GhostServicePlugin/ServiceTestForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GhostService.GhostServicePlugin
+{
+    partial class ServiceTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ServiceTestForm));
+            this.ssStatus = new System.Windows.Forms.StatusStrip();
+            this.tssLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbStatus = new System.Windows.Forms.ListBox();
+            this.ssStatus.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ssStatus
+            // 
+            this.ssStatus.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssLabel});
+            this.ssStatus.Location = new System.Drawing.Point(0, 247);
+            this.ssStatus.Name = "ssStatus";
+            this.ssStatus.Size = new System.Drawing.Size(498, 22);
+            this.ssStatus.TabIndex = 2;
+            this.ssStatus.Text = "statusStrip1";
+            // 
+            // tssLabel
+            // 
+            this.tssLabel.Name = "tssLabel";
+            this.tssLabel.Size = new System.Drawing.Size(38, 17);
+            this.tssLabel.Text = "Ready";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbStatus);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(498, 247);
+            this.panel1.TabIndex = 3;
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbStatus.FormattingEnabled = true;
+            this.lbStatus.Location = new System.Drawing.Point(12, 16);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lbStatus.Size = new System.Drawing.Size(474, 212);
+            this.lbStatus.TabIndex = 4;
+            // 
+            // ServiceTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 269);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ssStatus);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ServiceTestForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GhostService Setting Test";
+            this.ssStatus.ResumeLayout(false);
+            this.ssStatus.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip ssStatus;
+        private System.Windows.Forms.ToolStripStatusLabel tssLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox lbStatus;
+    }
+}
---
+++ trunk/GhostService/GhostServicePlugin/ServiceTestForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GhostService.GhostServicePlugin
+{
+    public partial class ServiceTestForm : Form
+    {
+        public ServiceTestForm()
+        {
+            InitializeComponent();
+        }
+
+        public void Add(string comment)
+        {
+            lbStatus.Items.Add(comment);                        
+        }
+
+        public void Status(string comment)
+        {
+            tssLabel.Text = comment;
+            ssStatus.Refresh();
+
+        }
+    }
+}
